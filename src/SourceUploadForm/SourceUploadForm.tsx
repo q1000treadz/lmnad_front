3e8db562b363f57e14e8084b6ed7f4b787c1c6da
macbookbro <macbookbro@MacBook-Air-macbookbro.local>
2024-06-02 22:59:45 +0300

    feat: paths

--- src/SourceUploadForm/SourceUploadForm.tsx
+++ src/SourceUploadForm/SourceUploadForm.tsx
@@ -33,7 +33,7 @@ const SourceUploadForm = ({ getDataSetter }: {getDataSetter: any}) => {
 
 		// Request made to the backend api
 		// Send formData object
-		axios.post('http://212.193.60.235' + "api/file", formData).then(res => {
+		axios.post('http://212.193.60.235' + "/api/file", formData).then(res => {
       if(res.status>=200 && res.status<300) {
         let data: any = {...formFields};
         console.log(res.data)
