f6c1a352d549d670dcfb68504863d332decf0f15
macbookbro <macbookbro@MacBook-Air-macbookbro.local>
2024-07-04 22:41:43 +0300

    feat: add .env

--- src/FileUploader/FileUploaderMultiple.tsx
+++ src/FileUploader/FileUploaderMultiple.tsx
@@ -21,7 +21,7 @@ function FileUploadMultiple({ getUploadedFiles }: {getUploadedFiles: any}) {
     });
 
     // 👇 Uploading the files using the fetch API to the server
-    fetch('http://212.193.60.235' + "/api/file", {
+    fetch(process.env.REACT_APP_BACKEND_URL + "/file", {
       method: 'POST',
       body: data,
     })
--- src/Login/Login.tsx
+++ src/Login/Login.tsx
@@ -12,7 +12,7 @@ const Login = () => {
     e.preventDefault();
     const user = { login, password };
     try {
-    const res = await axios.post('http://212.193.60.235/api/auth/login', user);
+    const res = await axios.post(process.env.REACT_APP_BACKEND_URL + '/auth/login', user);
     if (res.data.access_token) {
       localStorage.setItem('token', res.data.access_token);
       navigate('/');
